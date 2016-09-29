34b29b17be407bca1fc5b1bd9fc93adfe8fd1789
Julian Avila <avilatas@gmail.com>
2016-09-28 23:07:59 -0500

    Migraciones

---
+++ database/migrations/2016_09_29_040012_create_movies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+          $table->string('name');
+          $table->string('cast');
+          $table->string('direction');
+          $table->string('duration');
+          $table->integer('genre_id')->unsigned();
+          $table->foreign('genre_id')->references('id')->on('genres')->onDelete('cascade');;
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('movies');
+    }
+}
